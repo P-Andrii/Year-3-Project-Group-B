3c6e09195c8ed1962b282e3a1400f0330c5bd52b
FortySecond <147150865+P-Andrii@users.noreply.github.com>
2025-03-25 14:56:11 +0000

    Update motors.ino
    Removed button control
    On behalf of Will

--- motors.ino
+++ motors.ino
@@ -1,14 +1,10 @@
-// written by William
 // declare variables
 const int forwardPin = 8;
 const int backwardPin = 12;
+const int forwardPin2 = 9;
+const int backwardPin2 = 13;
 const int DelayTime = 2000;
-const int but1Pin = 2;
-const int but2Pin = 3;
 const int speedPin = 11;
-int but1;
-int but2;
-int pot;
 int speed;
 
 void setup()
@@ -16,37 +12,25 @@ void setup()
   Serial.begin(9600);
   pinMode(forwardPin, OUTPUT);
   pinMode(backwardPin, OUTPUT);
-  pinMode(but1Pin, INPUT_PULLUP);
-  pinMode(but2Pin, INPUT_PULLUP);
+  pinMode(forwardPin2, OUTPUT);
+  pinMode(backwardPin2, OUTPUT);
+  speed = 250;
+  analogWrite(speedPin,speed);
 }
 
 void loop()
 {
-  but1 = digitalRead(but1Pin);
-  but2 = digitalRead(but2Pin);
-
-  speed = 200;
-  analogWrite(speedPin,speed);
-
-
-  if(but1 == LOW){
-    digitalWrite(forwardPin,LOW);
-    Serial.print("Button 1 Pressed");
-    Serial.print(speed);
-    Serial.println();
-  }
-  else{
-    digitalWrite(forwardPin,HIGH);
-  }
-  if(but2 == LOW){
-    digitalWrite(backwardPin,LOW);
-    Serial.print("Button 2 Pressed, ");
-    Serial.print(speed);
-    Serial.println();
-  }
-  else{
-    digitalWrite(backwardPin,HIGH);
-  }
-
+  digitalWrite(forwardPin,LOW);
+  digitalWrite(forwardPin2,LOW);
+  delay(500);
+  digitalWrite(forwardPin,HIGH);
+  digitalWrite(forwardPin2,HIGH);
+  delay(500);
+  digitalWrite(backwardPin,LOW);
+  digitalWrite(backwardPin2,LOW);
+  delay(500);
+  digitalWrite(backwardPin,HIGH);
+  digitalWrite(backwardPin2,HIGH);
+  delay(500);
 
 }
