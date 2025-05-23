45e335a4a8476aa8c72c78710e52bffc34db8a42
FortySecond <147150865+P-Andrii@users.noreply.github.com>
2025-05-23 01:42:02 +0100

    Update transceiver.ino
    Updated for 8-slot data transmission array

--- transceiver.ino
+++ transceiver.ino
@@ -8,16 +8,15 @@ RF24 radio(9, 10);
 byte address[][6] = {"1Node","2Node","3Node","4Node","5Node","6Node"};
 bool transmitter = 1; // a boolean indicating whether this module is a transmitter (1) or a receiver (0)
 byte pipeNo, gotByte;
-int counter, input_data, data[2];
+int counter, input_data, data[8];
 
 void setup(){
   Serial.begin(9600);
-  data[0] = 2; data[1] = 4;
   radio.begin();
   radio.setAutoAck(1); // Automatically acknowledge received messages
   radio.setRetries(0,15);  // Time between and number of attempts of sending a message
   radio.enableAckPayload();   // Enable sending data as an acknowledgemt of receivement
-  radio.setPayloadSize(32);     // Package size in bytes
+  radio.setPayloadSize(64);     // Package size in bytes
   if (transmitter){radio.openWritingPipe(address[0]);}  // Writing to Address 0
   else{radio.openReadingPipe(1, address[0]);}       // Listening to Address 0
   
@@ -45,7 +44,24 @@ void loop(void) {
     while( radio.available(&pipeNo)){
       radio.read( &data, sizeof(data) );
 
-      Serial.print("Recieved: "); Serial.print(data[0]); Serial.println(data[1]);
+      Serial.print("Recieved: ");
+      Serial.print(data[0]);
+      Serial.print("   ");
+      Serial.print(data[1]);
+      Serial.print("   ");
+      Serial.print(data[2]);
+      Serial.print("   ");
+      Serial.print(data[3]);
+      Serial.print("   ");
+      Serial.print(data[4]);
+      Serial.print("   ");
+      Serial.print(data[5]);
+      Serial.print("   ");
+      Serial.print(data[6]);
+      Serial.print("   ");
+      Serial.print(data[7]);
+      Serial.print("   ");
+      Serial.println(data[8]);
     }
   }
 }
